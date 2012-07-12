7c15bbc4d26a67630f42864445bf49d89cf1675c
Ryan Hayes <ryanhayes84@gmail.com>
2012-07-12 16:03:44 -0400

    Added more apps.
    Added more apps to the list.

--- run.bat
+++ run.bat
@@ -1,3 +1,14 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
-echo "Setting up Filezilla"
 cinst filezilla
+cinst adobereader
+cinst putty
+cinst curl
+cinst githubforwindows
+cinst paint.net
+cinst skype
+cinst linqpad4
+cinst VirtualCloneDrive
+cinst kdiff3
+cinst GoogleChrome
+cinst evernote
+cinst SkyDrive
