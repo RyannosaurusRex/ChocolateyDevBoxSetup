e73289b7c94ee3668ecdd03333b0a593c4e66283
Ryan Hayes <ryanhayes84@gmail.com>
2012-07-12 15:47:51 -0400

    Initial commit

---
+++ run.bat
@@ -0,0 +1,3 @@
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+echo "Setting up Filezilla"
+cinst filezilla
